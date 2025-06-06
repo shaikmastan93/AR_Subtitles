f70440df055458e9235d932f14fe08c5bfb99fcc
Shaik Mastan <shaikmastan93@gmail.com>
2025-06-06 11:04:42 +0530

    resret code

--- ar.js
+++ ar.js
@@ -691,8 +691,6 @@ function handleOrientation(event) {
     // Landscape Mode
     subtitleContainer.classList.add('landscape');
     subtitleContainer.classList.remove('portrait');
-    subtitleContainer.style.top = '-50%'; // move down in landscape
-    // subtitleContainer.style.transform = `translate(-50%, -50%) rotate(${rotateDeg}deg)`;
     subtitleContainer.style.transform = `rotate(${rotateDeg}deg)`;
   }
 }
